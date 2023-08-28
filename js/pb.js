442095aca30da4af7eea89cf14b14577c0992266
ckdgus12 <ckdgus1308@naver.com>
2023-08-28 17:22:40 +0900

    28일 작업

--- js/pb.js
+++ js/pb.js
@@ -5,6 +5,8 @@ const getRandomRadius = () => Math.random() * 1 + 0.5;
 const getRandomSpeed = () => Math.random() * 0.3 + 0.1;
 const getRandomDir = () => [-1, 1][Math.floor(Math.random() * 2)];
 
+
+
 const Snow = {
   data: [],
   canvasWidth: $canvas.clientWidth,
@@ -92,27 +94,31 @@ window.onresize = () => {
 
 
 
+const backToTop = document.getElementById('backtotop');
 
-// swiper
-var swiper = new Swiper(".mySwiper", {
-  // loop: true,
-  slidesPerView: 1.3,
-  spaceBetween: 20,
-  breakpoints: {
-    600:{
-      slidesPerView: 2.4,  //브라우저가 768보다 클 때
-      spaceBetween: 30,
-    },
-    1100: {
-        slidesPerView: 3.8,  //브라우저가 768보다 클 때
-        spaceBetween: 30,
-    },
-    1400: {
-        slidesPerView: 4.8,  //브라우저가 1200보다 클 때
-        spaceBetween: 50,
-    }
+function checkScroll() {
+   
+  let pageYOffset = window.pageYOffset
+
+  if(pageYOffset !== 0) {
+    backToTop.classList.add('show');
+  }else{
+    backToTop.classList.remove('show');
   }
-});
+}
+
+function moveBackToTop() {
+   if (window.pageYOffset >0 ){
+
+
+    window.scrollTo({top: 0, behavior:"smooth"})
+   }
+}
+
+
+window.addEventListener('scroll', checkScroll);
+backToTop.addEventListener('click', moveBackToTop);
+
 
 
 
