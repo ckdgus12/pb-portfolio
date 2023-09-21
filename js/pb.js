d33f8045f2c32e291a99e6f71d9b752aa22b594e
ckdgus12 <ckdgus1308@naver.com>
2023-09-21 20:36:05 +0900

    로딩 화면 구현

--- js/pb.js
+++ js/pb.js
@@ -1,20 +1,77 @@
+  (() => {
+  const load = document.querySelector(".load");
+  const html = document.querySelector("html");
+  let scrollY = 0; 
+  let headerHeight = document.querySelector("header").offsetHeight;
+  const screenHeight = screen.availHeight;
+
+  html.style.overflow = "hidden";
+
+  window.onkeydown = function () {
+    const kcode = event.keyCode;
+    if (kcode == 116 || kcode == 82) {
+      window.location.reload(true);
+    }
+  };
+
+
+  window.addEventListener("load", () => {
+    scrollTo(0, 0);
+    setTimeout(() => {
+      load.classList.add("hide");
+      html.style.overflow = "auto";
+      load.style.opactiy = "2";
+    }, 3000);
+ 
+  });
+
+
+
+
+
+
+
+
+
 //내비게이션
-const menuIcon = document.querySelector("#menu_icon");
-const navBar = document.querySelector(".navbar");
-const navBg = document.querySelector(".nav_bg");
-const navLinks = document.querySelectorAll("header nav a");
+for (let i = 0; i < navLinks.length; i++){
+  let sectionOffsetTop = sections[i].offsetTop;
+  let sectionOffsetHeight = sections[i].offsetHeight;
+  
+
+  logoBtn.addEventListener("click", () =>{
+    window.scrollTo(0, 0);
+  });
+
+  navLinks[i].addEventListener("click", () => {
+    window.scrollTo(0, sections[i].offsetTop - headerHeight);
+  });
+  
+  // 첫 로딩
+  navLinks[0].classList.add("active");
+
+  window.addEventListener("scroll", () => {
+    scrollY = window.scrollY;
+    if(
+      scrollY >= sectionOffsetTop - headerHeight &&
+      scrollY < sectionOffsetTop + sectionOffsetHeight - headerHeight
+    ) {
+      navLinks[i].classList.add("active");
+    } else {
+      navLinks[i].classList.remove("active");
+    }
+  });
+}
 
 
+
+
+//햄버거 버튼
 menuIcon.addEventListener("click", (e) => {
   menuIcon.classList.toggle("bx-x");
   navBar.classList.toggle("active");
   navBg.classList.toggle("active");
   });
-
-//내비게이션 
-
-
-
 
 
 const backToTop = document.getElementById('backtotop');
@@ -44,7 +101,6 @@ backToTop.addEventListener('click', moveBackToTop);
 
 
 
-
 // swiper
 var swiper = new Swiper(".mySwiper", {
   // loop: true,
@@ -65,3 +121,5 @@ var swiper = new Swiper(".mySwiper", {
     }
   }
 });
+
+})();
