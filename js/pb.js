e3373b78679f58d2132cbea67cf4b06e1a3c311f
ckdgus12 <ckdgus1308@naver.com>
2023-10-30 21:08:29 +0900

    css 마무리

--- js/pb.js
+++ js/pb.js
@@ -194,7 +194,7 @@
         if (entry.isIntersecting) {
           entry.target.classList.add("show");
         } else {
-          entry.target.classList.remove("show");
+          /*entry.target.classList.remove("show");*/
         }
       });
     }, options);
