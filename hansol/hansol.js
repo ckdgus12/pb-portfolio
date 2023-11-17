049e48876b220ebf98b7d038ea456d84e72caa31
ckdgus12 <ckdgus1308@naver.com>
2023-11-17 22:05:47 +0900

    한솔

---
+++ hansol/hansol.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
